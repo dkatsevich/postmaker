88dcfffaec3b0b3a8c56f57276fdd5d4d8c23a2b
dkatsevich <d.katsevich.4@gmail.com>
2021-09-30 17:57:30 +0300

    Finish commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import FileBase from 'react-file-base64';
+
+import { createPost, updatePost } from '../../actions/posts';
+import useStyles from './styles';
+
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({ title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ title: '', message: '', tags: '', selectedFile: '' });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost({ ...postData, name: user?.result?.name }));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
+      clear();
+    }
+  };
+
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like other's memories.
+        </Typography>
+      </Paper>
+    );
+  }
+
+  const handleChange = (e) => {
+    setPostData({...postData, [e.target.name]: e.target.value})
+  }
+
+  return (
+    <Paper className={classes.paper}>
+      <form autoComplete="off" className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Post'}</Typography>
+        <TextField required={true} name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={handleChange} />
+        <TextField required={true} name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={handleChange} />
+        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={handleChange} />
+        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
+  );
+
+};
+
+export default Form;
